53574fd3694aaa8ffaaf56351b7a8756d7942776
Es180 <Es18.03.88@gmail.com>
2021-08-23 12:37:19 +0300

    refactoring

--- src/Users.tsx
+++ src/Users.tsx
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import {SearchResult, SearchUserType, UserType} from "./Github";
 import s from "./Github.module.css"
 import 'antd/dist/antd.css';
-import {Avatar, List, Spin } from "antd";
+import {Avatar, List, Spin} from "antd";
 import InfiniteScroll from 'react-infinite-scroller';
 
 
@@ -11,17 +11,12 @@ type UsersPropsType = {
     term: string
     selectedUser: SearchUserType | null
     onUserSelect: (user: SearchUserType) => void
-
-    // items: SearchUserType[]
 }
 
 export const Users = (props: UsersPropsType) => {
     const [users, setUsers] = useState<SearchUserType[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [hasMore, setHasMore] = useState<boolean>(false);
-    const [message, setMessage] = useState<string>("");
-    const [usersDetail, setUsersDetail] = useState<null | UserType>(null);
-
 
     useEffect(() => {
         axios
@@ -30,109 +25,26 @@ export const Users = (props: UsersPropsType) => {
                 setUsers(res.data.items)
             })
     }, [props.term]);
-    // state = {
-    //     users: [],
-    //     loading: false,
-    //     hasMore: true,
-    // };
-
-    useEffect(() => {
-
-    }, [])
-    // componentDidMount() {
-    //     this.fetchData(res => {
-    //         this.setState({
-    //             data: res.results,
-    //         });
-    //     });
-    // }
-    //
-    // fetchData = callback => {
-    //     reqwest({
-    //         url: fakeDataUrl,
-    //         type: 'json',
-    //         method: 'get',
-    //         contentType: 'application/json',
-    //         success: res => {
-    //             callback(res);
-    //         },
-    //     });
-    // };
-   const handleInfiniteOnLoad = () => {
-        // let { data } = props.items;
-            setLoading( true)
-        if (users.length > 14) {
-            setMessage('Infinite List loaded all');
-                setHasMore( false)
-                setLoading( false)
-            return;
-        }
-        // this.fetchData(res => {
-        //     data = data.concat(res.results);
-        //     this.setState({
-        //         data,
-        //         setLoading( false)
-        //     });
-        // });
-    };
-
 
     return <div>
-        {/*<Spin />*/}
         <div className="demo-infinite-container">
-            <InfiniteScroll
-                initialLoad={false}
-                pageStart={0}
-                loadMore={handleInfiniteOnLoad}
-                hasMore={!loading && hasMore}
-                useWindow={false}
-            >
-                <List
-                    dataSource={users}
-                    renderItem={item => (
-                        <List.Item key={props.selectedUser?.id}>
-                            <List.Item.Meta
-
-                                avatar={
-                                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                                }
-                                // title={usersDetail?.login}
-                                // description={usersDetail?.followers}
-
-                            />
-                            {users.map(u => <li
-                                key={u.id}
-                                className={props.selectedUser === u ? s.selected : ""}
-                                onClick={() => {
-                                    props.onUserSelect(u)
-                                }
-                                }
-                            >{u.login}</li>)}
-                            <div>Content</div>
-                        </List.Item>
-                    )
+            <ul>
+                {loading && hasMore && (
+                    <div className="demo-loading-container">
+                        <Spin/>
+                    </div>
+                )}
+                {users.map(u => <li
+                    key={u.id}
+                    className={props.selectedUser === u ? s.selected : ""}
+                    onClick={() => {
+                        props.onUserSelect(u)
+                    }
                     }
                 >
-
-                    {loading && hasMore && (
-                        <div className="demo-loading-container">
-                            <Spin />
-                        </div>
-                    )}
-                </List>
-            </InfiniteScroll>
+                    {/*<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />*/}
+                    {u.login}</li>)}
+            </ul>
         </div>
-
-
-        <ul>
-            {users.map(u => <li
-                key={u.id}
-                className={props.selectedUser === u ? s.selected : ""}
-                onClick={() => {
-                    props.onUserSelect(u)
-                }
-                }
-            >{u.login}</li>)}
-        </ul>
     </div>
 }
